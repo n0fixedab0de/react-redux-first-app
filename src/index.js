d1a866e84466c2848e3fb74f46a81252692d5ac4
Russell Webb <russell.c.webb@gmail.com>
2022-07-21 18:02:11 +0100

    Added Faker and demo comment JSX

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // 1. Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { faker } from "@faker-js/faker";
 
 // 2. Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -10,10 +11,24 @@ const root = ReactDOM.createRoot(el);
 
 // 4. Create a component
 function App() {
-    return <div>
-        My name is
-        <h1>Russell</h1>
-    </div>;
+    return (
+      <div className="ui container comments">
+          <div className="comment">
+              <a href="/" className="avatar">
+                  <img alt="avatar" />
+              </a>
+              <div className="content">
+                  <a href="/" className="author">
+                      Sam
+                  </a>
+                  <div className="metadata">
+                      <span className="date">Today at 6:00PM</span>
+                  </div>
+                  <div className="text">Nice blog post!</div>
+              </div>
+          </div>
+      </div>
+    );
 }
 
 // 5. Show the component on the screen
