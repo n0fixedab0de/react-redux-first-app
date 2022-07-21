d29882809b61384b06e9ecfe35a83b5d1d380950
Russell Webb <russell.c.webb@gmail.com>
2022-07-21 21:17:55 +0100

    Using props and children

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import {faker} from "@faker-js/faker";
 
-function CommentDetail() {
+function CommentDetail(props) {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}/>
+                <img alt="avatar" src={props.avatar}/>
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Sam
+                    {props.author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{props.comment}</div>
             </div>
         </div>
-    )
+    );
 }
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {faker} from "@faker-js/faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 // 2. Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -14,12 +15,24 @@ const root = ReactDOM.createRoot(el);
 function App() {
     return (
         <div className="ui container comments">
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            <ApprovalCard>
+                <div>
+                    <h3>Warning!</h3>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author={faker.name.firstName()} timeAgo="Today at 4:45PM" comment={faker.lorem.lines(1)} avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author={faker.name.firstName()} timeAgo="Today at 5:15PM" comment={faker.lorem.lines(1)} avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author={faker.name.firstName()} timeAgo="Today at 6:00PM" comment={faker.lorem.lines(1)} avatar={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
     );
 }
